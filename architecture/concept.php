da4601a8f0ebb1d91f8a340551d53beb67e064b3
Sumanai <sumanai@ya.ru>
2022-12-06 15:24:06 +0400

    Доделал архитектурную часть

--- architecture/concept.php
+++ architecture/concept.php
@@ -1,4 +1,8 @@
 <?php
+
+// Какой-то глобальный конфиг
+const SECRET_KEY_TYPE = 'file';
+
 class Concept
 {
     private $client;
@@ -21,5 +25,50 @@ class Concept
         });
 
         $promise->wait();
+    }
+
+    private function getSecretKey(): string
+    {
+        return AbstractKeysFactory::getFactory()->getSecretKey();
+    }
+}
+
+abstract class AbstractKeysFactory
+{
+    public static function getFactory()
+    {
+        return match (SECRET_KEY_TYPE) {
+            'file' => new FileKeyFactory(),
+            'db' => new DBKeyFactory(),
+            'memcache' => new MemcacheKeyFactory(),
+                // etc
+            default => throw new Exception('Unknown key storage type'),
+        };
+    }
+
+    abstract public function getSecretKey(): string;
+}
+
+class FileKeyFactory extends AbstractKeysFactory
+{
+    public function getSecretKey(): string
+    {
+        return 'key from some file';
+    }
+}
+
+class DBKeyFactory extends AbstractKeysFactory
+{
+    public function getSecretKey(): string
+    {
+        return 'key from some database';
+    }
+}
+
+class MemcacheKeyFactory extends AbstractKeysFactory
+{
+    public function getSecretKey(): string
+    {
+        return 'key from memory';
     }
 }
